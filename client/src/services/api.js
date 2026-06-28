c5f3be8291a98911b793b6358455c0def6d24a14
Nathiya <nathiyamanickam6181@gmail.com>
2026-06-28 11:59:49 +0530

    clearmessage

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('user') || '{}');
+
   if (user.token) {
     req.headers.Authorization = `Bearer ${user.token}`;
   }
+
   return req;
 });
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 export const registerUser = (data) => API.post('/auth/register', data);
 export const loginUser = (data) => API.post('/auth/login', data);
 export const startInterview = (data) => API.post('/interview/start', data);
